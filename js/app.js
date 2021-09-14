f67e1889884e82aa14cf06229b6367fa502587a1
Ahmed Foysal <ahmedfoysal01797@gmail.com>
2021-09-14 20:21:24 +0600

    add icon

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const showProducts = (products) => {
       <p>Rating: ${product.rating.rate}</p>
       <p>Reviews: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart <span><i class="fas fa-cart-plus"></i></span></button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
